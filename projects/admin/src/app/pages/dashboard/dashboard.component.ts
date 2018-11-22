9272481577af9ee6a7b6a82931e3d4c438b9a247
sivaraj <sivaraj.s7@gmail.com>
2018-11-22 18:14:49 +0530

    22-11-2018

--- projects/admin/src/app/pages/dashboard/dashboard.component.ts
+++ projects/admin/src/app/pages/dashboard/dashboard.component.ts
@@ -9,104 +9,100 @@ import { MultipleDatePickerComponent } from '../../multiple-date-picker/multiple
 import { DateRangeHelper } from '../../multiple-date-picker/date-range-helper';
 
 @Component({
-	selector: 'app-dashboard',
-	templateUrl: './dashboard.component.html',
-	styleUrls: ['./dashboard.component.scss']
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
 
-	highlightDays: any[];
-	initialCount: Array<any>; // this is the [(ngModel)] property
-	datesArray: Array<any>;
-	myMonth: any;
+  highlightDays: any[];
+  initialCount: Array<any>; // this is the [(ngModel)] property
+  datesArray: Array<any>;
+  myMonth: any;
 
-	testItems: any[] = [
-		{ 'item': 'array1', 'id': 1 },
-		{ 'item': 'array2', 'id': 2 },
-		{ 'item': 'array3', 'id': 3 },
-	]  // potential use if person wanted to create a choosen item to associate with a list of arrays
+  testItems: any[] = [
+    { 'item': 'array1', 'id': 1 },
+    { 'item': 'array2', 'id': 2 },
+    { 'item': 'array3', 'id': 3 },
+  ]  // potential use if person wanted to create a choosen item to associate with a list of arrays
 
-	constructor() { }
+  constructor() { }
 
-	// * you don't have to use @ViewChild as it this is used if you want to use some functions within the
-	// multiple-date-picker that will help in testing and construction of your design.
-	// For example: findArrayofDays() { console.log('this.projectScope = ' + this.projectScope); }
-	// clearDays() {
-	// this.projectScope = []; this.generate();
-	// 	console.log('clearDays was fired off');
-	// }
-	// and runGenerate() { this.generate(); } 
-	// remove this and from html
-	@ViewChild(MultipleDatePickerComponent) private multipleDatePicker: MultipleDatePickerComponent;
+  // * you don't have to use @ViewChild as it this is used if you want to use some functions within the
+  // multiple-date-picker that will help in testing and construction of your design.
+  // For example: findArrayofDays() { console.log('this.projectScope = ' + this.projectScope); }
+  // clearDays() {
+  // this.projectScope = []; this.generate();
+  // 	console.log('clearDays was fired off');
+  // }
+  // and runGenerate() { this.generate(); }
+  // remove this and from html
+  @ViewChild(MultipleDatePickerComponent) private multipleDatePicker: MultipleDatePickerComponent;
 
-	ngOnInit(): void {
-		// this.heroService.getHeroes()
-		// 	.then(heroes => this.heroes = heroes.slice(1, 5));
-		// console.log('this is here = ' + matIcons);
+  ngOnInit(): void {
+    // set array to either [] or the following values like example below
+    this.highlightDays = [
+      { date: moment().date(22).valueOf(), css: 'holiday', selectable: true, title: 'Holiday time !' },
+      { date: moment().date(25).valueOf(), css: 'off', selectable: false, title: 'We don\'t work today' },
+      { date: moment().date(30).valueOf(), css: 'birthday', selectable: true, title: 'I\'m thir... i\'m 28, seriously, I mean ...' }
+    ];
+    console.log(this.highlightDays);
+    // console.log(new Date(1532522820921));
+    // examples to work with
+    // console.log('date: moment().date(19).valueOf() ', moment().date(19).valueOf());
+    // console.log('date: moment().date(20).valueOf() ', moment().date(20).valueOf());
+    // console.log('date: moment().date(21).valueOf() ', moment().date(21).valueOf());
 
-		// set array to either [] or the following values like example below
-		this.highlightDays = [
-			{ date: moment().date(22).valueOf(), css: 'holiday', selectable: true, title: 'Holiday time !' },
-			{ date: moment().date(25).valueOf(), css: 'off', selectable: false, title: 'We don\'t work today' },
-			{ date: moment().date(30).valueOf(), css: 'birthday', selectable: true, title: 'I\'m thir... i\'m 28, seriously, I mean ...' }
-		];
-		console.log(this.highlightDays);
-		// console.log(new Date(1532522820921));
-		// examples to work with
-		// console.log('date: moment().date(19).valueOf() ', moment().date(19).valueOf());
-		// console.log('date: moment().date(20).valueOf() ', moment().date(20).valueOf());
-		// console.log('date: moment().date(21).valueOf() ', moment().date(21).valueOf());
+    // ***Setting project scope.
+    this.initialCount = [];
 
-		// ***Setting project scope.
-		this.initialCount = [];
+    // enter variables for startDates and End dates
+    let startDate = moment().date(20).valueOf(); // enter variable or ms value
+    let endDate = moment().date(23).valueOf(); // enter variable or ms value // 1502510400000
 
-		// enter variables for startDates and End dates
-		let startDate = moment().date(20).valueOf(); // enter variable or ms value
-		let endDate = moment().date(23).valueOf(); // enter variable or ms value // 1502510400000
+    // console.log('calucator values ' + this.dateRangeHelper.dateRangeDaysCalculator(endDate, startDate))
 
-		// console.log('calucator values ' + this.dateRangeHelper.dateRangeDaysCalculator(endDate, startDate))
+    if (DateRangeHelper.dateRangeDaysCalculator(endDate, startDate) >= 0) {
+      let days = DateRangeHelper.dateRangeDaysCalculator(endDate, startDate);
+      this.datesArray = DateRangeHelper.getDates(new Date(startDate), (new Date(startDate)).addDays(days)); // date object used not moment in this case
+      console.log('this.datesArray ', this.datesArray);
+    }
 
-		if (DateRangeHelper.dateRangeDaysCalculator(endDate, startDate) >= 0) {
-			let days = DateRangeHelper.dateRangeDaysCalculator(endDate, startDate);
-			this.datesArray = DateRangeHelper.getDates(new Date(startDate), (new Date(startDate)).addDays(days)); // date object used not moment in this case
-			console.log('this.datesArray ', this.datesArray);
-		}
+    // takes array dates from daterangehelper and adds them to highlighted days for date picker day highlights
+    if (this.datesArray !== undefined && this.datesArray.length > 0) {
+      let daysArray = this.datesArray;
+      let arrayObject = daysArray.find(x => x);
+      let arrayKeys = Object.keys(daysArray);
+      if (arrayObject !== undefined && arrayKeys.length > 0) {
+        this.highlightDays = this.datesArray;
+        let stayNames = 'Christian Smith' // should be set to variable
+        for (let i in daysArray) {
+          if (true) {
+            console.log(daysArray);
+            // this.highlightDays.push({ date: daysArray[i], css: 'stay-dates', selectable: true, title: `days off for ${stayNames}` });  // set strings
+            this.highlightDays.push({ date: daysArray[i], css: 'stay-dates', selectable: true, title: 'OFF' });  // set strings
+          }
+        }
+      }
+    }
 
-		// takes array dates from daterangehelper and adds them to highlighted days for date picker day highlights
-		if (this.datesArray !== undefined && this.datesArray.length > 0) {
-			let daysArray = this.datesArray;
-			let arrayObject = daysArray.find(x => x);
-			let arrayKeys = Object.keys(daysArray);
-			if (arrayObject !== undefined && arrayKeys.length > 0) {
-				this.highlightDays = this.datesArray;
-				let stayNames = 'Christian Smith' // should be set to variable 
-				for (let i in daysArray) {
-					if (true) {
-						console.log(daysArray);
-						// this.highlightDays.push({ date: daysArray[i], css: 'stay-dates', selectable: true, title: `days off for ${stayNames}` });  // set strings
-						this.highlightDays.push({ date: daysArray[i], css: 'stay-dates', selectable: true, title: 'OFF' });  // set strings
-					}
-				}
-			}
-		}
+    // calculate addional months to add onto the month object... if this is corrupt in anyway it will default to todays month info
+    // let monthsFromToday = DateRangeHelper.dateRangeMonthsCalculator(startDate);
+    // if (monthsFromToday > 0) {
+    // 	// this.myMonth = moment().add(monthsFromToday, 'months');
+    // } else {
+    // 	this.myMonth = moment().startOf('day');
+    // }
 
-		// calculate addional months to add onto the month object... if this is corrupt in anyway it will default to todays month info
-		// let monthsFromToday = DateRangeHelper.dateRangeMonthsCalculator(startDate);
-		// if (monthsFromToday > 0) {
-		// 	// this.myMonth = moment().add(monthsFromToday, 'months');
-		// } else {
-		// 	this.myMonth = moment().startOf('day');
-		// }
+  }
 
-	}
+  logMonthChanged(newMonth, oldMonth) {
+    console.log(newMonth);
+  }
 
-	logMonthChanged(newMonth, oldMonth) {
-		console.log(newMonth);
-	}
-
-	oneDaySelectionOnly(event, date) {
-		console.log(event);
-		this.highlightDays.push({ date: date.date._d, css: 'stay-dates', selectable: true, title: 'days off for'});
-		console.log(this.highlightDays);
-	}
+  oneDaySelectionOnly(event, date) {
+    console.log(event);
+    this.highlightDays.push({ date: date.date._d, css: 'stay-dates', selectable: true, title: 'days off for' });
+    console.log(this.highlightDays);
+  }
 }
--- projects/admin/src/app/pages/jobs/job-details/job-details.component.ts
+++ projects/admin/src/app/pages/jobs/job-details/job-details.component.ts
@@ -29,87 +29,40 @@ export class JobDetailsComponent implements OnInit {
 
   public ELEMENT_DATA: any[] = [
     {
-      position: 1,
-      name: 'Hydrogen',
-      weight: 1.0079,
-      symbol: 'H',
+      name: 'name',
+      jan1stHalf: {
+        unVerified: 6,
+      },
+      jan2ndHalf: 'H',
+      feb1stHalf: 'f',
+      feb2ndHalf: 'g',
+      mar1stHalf: 'l',
+      mar2ndHalf: 'f',
+      apr1stHalf: 't',
+      apr2ndHalf: 'e',
+      may1stHalf: 'r',
+      may2ndHalf: 't',
+      jun1stHalf: 'e',
+      jun2ndHalf: 'q',
+      jul1stHalf: 'j',
+      jul2ndHalf: 'q',
+      aug1stHalf: 'a',
+      aug2ndHalf: 'e',
+      sep1stHalf: 'w',
+      sep2ndHalf: 's',
+      oct1stHalf: 'o',
+      oct2ndHalf: 'k',
+      nov1stHalf: 'n',
+      nov2ndHalf: 'm',
+      dec1stHalf: 'd',
+      dec2ndHalf: 'f',
       description: `Hydrogen is a chemical element with symbol H and atomic number 1. With a standard
         atomic weight of 1.008, hydrogen is the lightest element on the periodic table.`
-    }, {
-      position: 2,
-      name: 'Helium',
-      weight: 4.0026,
-      symbol: 'He',
-      description: `Helium is a chemical element with symbol He and atomic number 2. It is a
-        colorless, odorless, tasteless, non-toxic, inert, monatomic gas, the first in the noble gas
-        group in the periodic table. Its boiling point is the lowest among all the elements.`
-    }, {
-      position: 3,
-      name: 'Lithium',
-      weight: 6.941,
-      symbol: 'Li',
-      description: `Lithium is a chemical element with symbol Li and atomic number 3. It is a soft,
-        silvery-white alkali metal. Under standard conditions, it is the lightest metal and the
-        lightest solid element.`
-    }, {
-      position: 4,
-      name: 'Beryllium',
-      weight: 9.0122,
-      symbol: 'Be',
-      description: `Beryllium is a chemical element with symbol Be and atomic number 4. It is a
-        relatively rare element in the universe, usually occurring as a product of the spallation of
-        larger atomic nuclei that have collided with cosmic rays.`
-    }, {
-      position: 5,
-      name: 'Boron',
-      weight: 10.811,
-      symbol: 'B',
-      description: `Boron is a chemical element with symbol B and atomic number 5. Produced entirely
-        by cosmic ray spallation and supernovae and not by stellar nucleosynthesis, it is a
-        low-abundance element in the Solar system and in the Earth's crust.`
-    }, {
-      position: 6,
-      name: 'Carbon',
-      weight: 12.0107,
-      symbol: 'C',
-      description: `Carbon is a chemical element with symbol C and atomic number 6. It is nonmetallic
-        and tetravalent—making four electrons available to form covalent chemical bonds. It belongs
-        to group 14 of the periodic table.`
-    }, {
-      position: 7,
-      name: 'Nitrogen',
-      weight: 14.0067,
-      symbol: 'N',
-      description: `Nitrogen is a chemical element with symbol N and atomic number 7. It was first
-        discovered and isolated by Scottish physician Daniel Rutherford in 1772.`
-    }, {
-      position: 8,
-      name: 'Oxygen',
-      weight: 15.9994,
-      symbol: 'O',
-      description: `Oxygen is a chemical element with symbol O and atomic number 8. It is a member of
-         the chalcogen group on the periodic table, a highly reactive nonmetal, and an oxidizing
-         agent that readily forms oxides with most elements as well as with other compounds.`
-    }, {
-      position: 9,
-      name: 'Fluorine',
-      weight: 18.9984,
-      symbol: 'F',
-      description: `Fluorine is a chemical element with symbol F and atomic number 9. It is the
-        lightest halogen and exists as a highly toxic pale yellow diatomic gas at standard
-        conditions.`
-    }, {
-      position: 10,
-      name: 'Neon',
-      weight: 20.1797,
-      symbol: 'Ne',
-      description: `Neon is a chemical element with symbol Ne and atomic number 10. It is a noble gas.
-        Neon is a colorless, odorless, inert monatomic gas under standard conditions, with about
-        two-thirds the density of air.`
-    },
+    }
   ];
-  displayedColumns1: string[] = ['select1', 'jan', 'feb', 'mar', 'apr', 'may', 'june', 'july', 'aug', 'sep', 'oct', 'nov', 'dec'];
-  displayedColumns: string[] = ['select', 'jan 1st Half', 'jan 2nd Half', 'feb 1st Half', 'feb 2nd Half', 'mar 1st Half', 'mar 2nd Half', 'apr 1st Half', 'apr 2nd Half', 'may 1st Half', 'may 2nd Half', 'jun 1st Half', 'jun 2nd Half', 'jul 1st Half', 'jul 2nd Half', 'aug 1st Half', 'aug 2nd Half', 'sep 1st Half', 'sep 2nd Half', 'oct 1st Half', 'oct 2nd Half', 'nov 1st Half', 'nov 2nd Half', 'dec 1st Half', 'dec 2nd Half'];
+
+  displayedColumns1: string[] = ['select1', 'name1', 'jan', 'feb', 'mar', 'apr', 'may', 'june', 'july', 'aug', 'sep', 'oct', 'nov', 'dec'];
+  displayedColumns: string[] = ['select', 'name', 'jan1stHalf', 'jan2ndHalf', 'feb1stHalf', 'feb2ndHalf', 'mar1stHalf', 'mar2ndHalf', 'apr1stHalf', 'apr2ndHalf', 'may1stHalf', 'may2ndHalf', 'jun1stHalf', 'jun2ndHalf', 'jul1stHalf', 'jul2ndHalf', 'aug1stHalf', 'aug2ndHalf', 'sep1stHalf', 'sep2ndHalf', 'oct1stHalf', 'oct2ndHalf', 'nov1stHalf', 'nov2ndHalf', 'dec1stHalf', 'dec2ndHalf'];
   dataSource = new MatTableDataSource<any>(this.ELEMENT_DATA);
   selection = new SelectionModel<any>(true, []);
   expandedElement: any;
